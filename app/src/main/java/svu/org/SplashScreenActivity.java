30a9299fb56f364454feb93e5bfceb5a7f5529c1
Marya_AI <rahaf.m.alnajjar@gmail.com>
2019-06-15 14:04:48 +0300

    New design

--- app/src/main/java/svu/org/SplashScreenActivity.java
+++ app/src/main/java/svu/org/SplashScreenActivity.java
@@ -1,19 +1,33 @@
 package svu.org;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Intent intent = new Intent(getApplicationContext(),
-                MainActivity.class);
-        startActivity(intent);
-        finish();
+      //  Intent intent = new Intent(getApplicationContext(),
+      //          MainActivity.class);
+      //  startActivity(intent);
+       // finish();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
-}
+    }
